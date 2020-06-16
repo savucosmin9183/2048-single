d0944b2ea6fe428332daaf8bc7809049a6ce3909
cosmin savu <savucosmin9183@gmail.com>
2020-06-16 13:01:38 +0300

    test

---
+++ src/components/Game.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <GameGrid :name="name"/>
+    </div>
+</template>
+
+<script>
+import GameGrid from './GameGrid'
+
+export default {
+    name: 'Game',
+    props: ['name'],
+    data() {
+        return {
+            online_users: []
+        }
+    },
+    methods: {
+        exit(){
+        
+        }
+    },
+    created() {
+        window.addEventListener('beforeunload', this.exit);
+        window.addEventListener('popstate', this.exit);
+    },
+    beforeDestroy() {
+        window.removeEventListener('beforeunload', this.exit)
+        window.removeEventListener('popstate', this.exit)
+    },
+    components: {
+        GameGrid
+    }
+}
+</script>
+
+<style>
+   
+   .online_users p{
+       margin: 0;
+}
+</style>
